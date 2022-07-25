7c4c782590a29aede9ec777028e475eebbb68fdf
Cristina <cristinayunesm@gmail.com>
2022-07-25 00:04:18 -0400

    Second commit, password length prompt is working now

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passwordLength = "";
+
+
+// // 1. Password Criteria
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(",")"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var criteria ;
+var finalArray;
 
 
-// // 1. Password Criteria
 //   // 1.2. Password length from 8 to 128 characters
- var passwordLength = prompt("Please select in between 8 and 128 characters");
+function generatePassword() {
 
-if (passwordLength <8 || passwordLength >128) {
- window.alert("Invalid length, please choose between 8 and 128 characters");
+  var passwordInput = window.prompt("Please type a number between 8 and 128")
 
+  if (!passwordInput) {
+    window.alert("You must enter a number!");
+  } else if (isNaN(passwordInput) || passwordInput < 8 || passwordInput > 128 ) {
+    window.alert("Invalid, please type a number between 8 and 128") 
+  } else {
+    var mssgSymb = window.confirm("Include symbols?");
+    var mssgNum = window.confirm("Include numbers?");
+    var mssgLowerCase = window.confirm("Include lower case letters?");
+    var mssgUpperCase = window.confirm("Include upper case letters?");
+  }
+
+  if (passwordInput && !mssgSymb && !mssgNum && !mssgLowerCase && !mssgUpperCase) {
+    window.alert("You must pick at least one of the criterias!!");
+  } else if (mssgSymb && mssgNum && mssgLowerCase && mssgUpperCase) {
+    criteria = symbols.concat(numbers, uppercase, lowercase);
+  }
+
+  finalArray = [];
+
+  for (var i = 0; i < passwordInput; i++) {
+    var finalPicks = criteria[Math.floor(Math.random() * criteria.length)];
+    password.push(finalPicks);
 }
-console.log(passwordLength);
+
+// var password = finalArray.join("");
+// writePassword(password)
 
 
 
@@ -26,37 +52,9 @@ console.log(passwordLength);
 // // 3. Generate password
 // // 4. Display password
 
-// return "Generated password will be here"
-// }
 
-// function generateMessages () {
-
-//   var valid = false
-
-//   while {
-
-//   var lengthmssg = window.prompt("Select in between 8 and 128 characters");
-//   var symmssg = window.openDialog("Include symbols?");
-//   var nummssg = window.openDialog("Include numbers?");
-//   var symmssg = window.prompt("Include symbols?");
-
-// }
-
-
-  
-
-// function generatePassword() {
-//   console.log(passlength);
-
-//     return   
-// }
-
-
-// var confirmnumber = 
-// if (window.confirm("Do you really want to leave?") {
-//  window.open("exit.html", "Thanks for Visiting!");
-//  }
-
+return criteria;
+}
 
 // Write password to the #password input (original file)
 function writePassword() {
