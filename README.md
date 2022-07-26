41f4f5ce349b4522c04324def0b8059f7cba8aa1
Cristina <cristinayunesm@gmail.com>
2022-07-25 22:13:37 -0400

    Final commit, I was able to get help during office hours in order to debug my final code

--- README.md
+++ README.md
@@ -1,18 +1,4 @@
-# 03 JavaScript: Password Generator
-
-## Your Task
-
-This week's Challenge requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
-
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
-
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+# The JavaScript Password Generator Challenge
 
 ## Acceptance Criteria
 
@@ -34,67 +20,13 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 ```
 
-## Mock-Up
+## About the Project
 
-The following image shows the web application's appearance and functionality:
+The first week of learning JavaScript was pretty though and as you can see inside the JS file, I have commented everything that happened with my code. 
 
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+Please refer to the [script.js file in order to view the comments](/Users/cristinayunes/Desktop/03-javascript-challenge/assets/script.js)
 
-## Grading Requirements
-
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
+**Please View the Final page here: [View my Password Generator](https://yuyitax.github.io/03-js-challenge/)
 
 - - -
-© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+© 2022 Cristina Yunes. All Rights Reserved.
--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-// // 1. Password Criteria
+// // 1. Password Character Criteria
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(",")"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var criteria ;
-var finalArray;
-
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// var criteria; -- old variable
+var chars = []
 
 //   // 1.2. Password length from 8 to 128 characters
 function generatePassword() {
@@ -28,41 +27,102 @@ function generatePassword() {
     var mssgUpperCase = window.confirm("Include upper case letters?");
   }
 
-  if (passwordInput && !mssgSymb && !mssgNum && !mssgLowerCase && !mssgUpperCase) {
-    window.alert("You must pick at least one of the criterias!!");
-  } else if (mssgSymb && mssgNum && mssgLowerCase && mssgUpperCase) {
-    criteria = symbols.concat(numbers, uppercase, lowercase);
+  if (mssgSymb) {
+    chars = chars.concat(symbols)
   }
 
-  finalArray = [];
+  if (mssgNum) {
+    chars = chars.concat(numbers)
+  }
 
+  if (mssgLowerCase) {
+    chars = chars.concat(lowerCase)
+  }
+
+  if (mssgUpperCase) {
+    chars = chars.concat(upperCase)
+  }
+
+
+  // The following code is what I was doing before I requested help and understood that the if statements can be simplified much more like it is shown with the code above
+
+
+  // if (passwordInput && !mssgSymb && !mssgNum && !mssgLowerCase && !mssgUpperCase) {
+  //   window.alert("You must pick at least one of the criterias!!");
+  //   // Adding the 4 criterias
+  // } else if (mssgSymb && mssgNum && mssgLowerCase && mssgUpperCase) {
+  //   criteria = symbols.concat(numbers, upperCase, lowerCase);
+  //   // Removing one of the criterias
+  //   // If upperCase is not picked
+  // } else if (mssgSymb && mssgNum && mssgLowerCase && !mssgUpperCase) {
+  //   criteria = symbols.concat(numbers, lowerCase);
+  //    // If lowerCase is not picked
+  // } else if (mssgSymb && mssgNum && !mssgLowerCase && mssgUpperCase) {
+  //   criteria = symbols.concat(numbers, upperCase);
+  //    // If mssgNum is not picked
+  // } else if (mssgSymb && !mssgNum && mssgLowerCase && mssgUpperCase) {
+  //   criteria = symbols.concat(lowerCase, upperCase);
+  //    // If mssgSymb is not picked
+  // } else if (!mssgSymb && mssgNum && mssgLowerCase && mssgUpperCase) {
+  //   criteria = numbers.concat(lowerCase, upperCase);
+
+  //   // Removing two of the criterias
+  //   // If mssgLowerCase and mssgUpperCase are not picked
+  // } else if (mssgSymb && mssgNum) {
+  //   criteria = symbols.concat(mssgNum);
+  //   // If mssgLowerCase and mssgNum are not picked
+  // } else if (mssgSymb && mssgUpperCase) {
+  //   criteria = symbols.concat(upperCase);
+  //    // If mssgNum and mssgUpperCase are not picked
+  // } else if (mssgSymb && mssgLowerCase) {
+  //   criteria = symbols.concat(lowerCase);
+  //    // If mssgLowerCase and mssgSymb are not picked
+  // } else if (mssgNum && mssgUpperCase) {
+  //   criteria = numbers.concat(upperCase);
+  //    // If mssgSymb and mssgUpperCase are not picked
+  // } else if (mssgNum && mssgLowerCase) {
+  //   criteria = numbers.concat(lowerCase);
+  //   // If mssgNum and mssgSymb are not picked
+  // } else if (mssgLowerCase && mssgUpperCase) {
+  //   criteria = mssgLowerCase.concat(upperCase);
+
+  //   // Picking only ONE criteria
+  //   // Only mssgSymb is picked
+  // } else if (mssgSymb) {
+  //   criteria = symbols;
+  //   // Only mssgNum is picked
+  // } else if (mssgNum) {
+  //   criteria = numbers;
+  //   // Only mssgLowerCase is picked
+  // } else if (mssgLowerCase) {
+  //   criteria = lowerCase;
+  //   // Only mssgUpperCase is picked
+  // } else if (mssgUpperCase) {
+  //   criteria = upperCase;
+  // }
+
+ // 2. Validate input
+ // 3. Generate password
+  var password = []
   for (var i = 0; i < passwordInput; i++) {
-    var finalPicks = criteria[Math.floor(Math.random() * criteria.length)];
-    password.push(finalPicks);
-}
+    var randomPick = Math.floor(Math.random() * chars.length)
+    var randomChar = chars[randomPick]
+    password.push(randomChar);
+  }
 
-// var password = finalArray.join("");
-// writePassword(password)
+  var result = password.join("")
+  console.log(result)
 
+  return result
 
-
-//   // 1.3. Lowercase, uppercase, numeric, and/or special characters
-  
-// // 2. Validate input
-// // 3. Generate password
-// // 4. Display password
-
-
-return criteria;
 }
 
 // Write password to the #password input (original file)
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); // you're calling generatePassword - that function should return something
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button (original file)
